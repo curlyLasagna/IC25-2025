8db2d23bf85b463c2ec6148146f3d84c47481e3c
Andreas <git@fsck.rip>
2025-03-03 11:13:12 -0500

    refactor keyword loading logic to get data from csv
    Signed-off-by: Andreas <git@fsck.rip>

--- classification.py
+++ classification.py
@@ -5,47 +5,44 @@ import csv
 REQUESTS_FILE = "./data/data.csv"
 DEPARTMENTS_FILE = "./data/departments.csv"
 
-def match_categories(foia_description: str) -> str:
+
+def initialize_foia_keyword_map():
+    """
+    Load a list of keywords then map them to their
+    corresponding department, and designated POC within a tuple.
+    """
+
+    keyword_dict = {}
+    file = open(DEPARTMENTS_FILE, "r", encoding="utf8")
+    reader = csv.reader(file)
+
+    # Column order: Department, Keywords, Name
+    for i, row in enumerate(reader):
+        department = row[0]
+        poc_name = row[2]
+        
+        # skip the first row
+        if i == 0:
+            continue
+
+        # Load the keywords if the department is not in the map
+        if department not in keyword_dict:
+            keyword_dict[tuple([department, poc_name])] = row[1]
+
+    file.close()
+    return keyword_dict
+
+
+def match_categories(foia_description: str, categories: dict) -> str:
     """Get category matches based off a FOIA description."""
 
-    # Some of these are split()'d because they were written Excel
-    # rather than being hard-coded into the program, like it is now.
-    #
-    # There is a better way to do this but because we only have a week,
-    # this is the only "good" way without putting in a *lot* of effort.
-    categories = {
-        "Video - Locomotive": "dashcam,dash cam".split(","),
-        "Video - Surveillance/APD": "video,body cam,surveillance".split(","),
-        "Lawsuits": "lawsuit,lawsuits,law suit,law suits".split(","),
-        "Amtrak PD": "police reports,police,use-of-force".split(","),
-        "Audits": ["OIG", "Office of the Inspector General"],
-        "Claims": ["baggage claim", "claims"],
-        "Employee Medical Records": ["medical disqualification"],
-        "Procurement": "procurement,solicitation".split(","),
-        "Train Fares": "fee,fees".split(","),
-        "Ridership Statistics, Station Ridership Data": ["statistics"],
-        "Noise Assessments": "noise assessment,noise assessments".split(","),
-        "Central Reporting": "accident,incident,train incident,train accident".split(
-            ","
-        ),
-        "Contract Management": "bid,bid solicitation,contracts,contractors,contract,contactor".split(
-            ","
-        ),
-        "Comptroller/Finance": "financial,finance".split(","),
-        "Email Searches (eDiscovery)": ["emails", "internal emails"],
-        "Telephone Records": ["telephone"],
-        "Train Scheduling": ["train delays", "delays", "delay"],
-    }
-
+    categories = initialize_foia_keyword_map()
     v_adjectives = []
     for word in foia_description.split(" "):
-        for adj, keywords in categories.items():
-            if word in keywords:
-                if adj not in v_adjectives:
-                    v_adjectives.append(adj)
-
-        # print(foia_description)
-        # v_adjectives.append([adjective, foia_id])
+        for dept, dept_keywords in categories.items():
+            if word in dept_keywords:
+                if dept not in v_adjectives:
+                    v_adjectives.append(f"{dept[0]} - {dept[1]}")
 
     # If we don't have any adjectives, the report is not known.
     if len(v_adjectives) < 1:
@@ -58,19 +55,24 @@ def match_categories(foia_description: str) -> str:
     #     print(entry)
 
 
-def main(data):
+def main(data, dept_keywords: dict) -> None:
     """
     This script is responsible for classifying each request with
     the appropriate category based on keywords that specified below.
     Each keyword corresponds with a specific "category" (department)
     that is equipped with handling said report
     """
-    foias = open(data, "r")
+    foias = open(data, "r", encoding="utf8")
     reader = csv.reader(foias, delimiter=",")
     for row in reader:
-        print(f"{row[0]} {match_categories(row[1])}")
-    # match_category("hi")
-    # return categories
+        print(f"{row[0]} {match_categories(row[1], dept_keywords)}")
+
+    foias.close()
 
 
-main(REQUESTS_FILE)
+if __name__ == "__main__":
+    # Load keywords into memory prior to doing anything.
+    keywords = initialize_foia_keyword_map()
+
+
+    main(REQUESTS_FILE, keywords)
